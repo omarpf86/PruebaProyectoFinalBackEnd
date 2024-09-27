b7f867ba6d796f399c07376d0167a340774adb06
omarpf86 <omarpf86@gmail.com>
2024-09-27 17:03:34 -0300

    Delete Cuarta_pre_entrega_2_Perez_Fortunato_Omar directory

--- Cuarta_pre_entrega_2_Perez_Fortunato_Omar/src/middlewares/isAuth.js
+++
@@ -1,7 +0,0 @@
-export const isAuth = (req, res, next) => {
-    console.log("Authenticated user in isAuth:", req.user);
-    if (req.session.passport && req.session.passport.user) {
-        if (req.isAuthenticated()) return next();
-    }
-    res.status(403).json({ msg: "No Autorizado" });
-};
--- Cuarta_pre_entrega_2_Perez_Fortunato_Omar/src/persistence/dtos/user.res.dto2.js
+++
@@ -1,7 +0,0 @@
-export default class UserResDTO2 {
-    constructor(user) {
-        this.nombre = user.first_name;
-        this.email = user.email;
-        this.rol = user.role
-    }
-}
